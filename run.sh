962c55c1c1f46ec25287e3c96ebfe4e1d1e1abbd
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2020-08-28 19:36:45 -0300

    🔧 Change python dependencies install

--- run.sh
+++ run.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 set -e
 
-wget -O split.py https://raw.githubusercontent.com/northy/pdf-to-markdown-workflow/master/split.py
+echo "Fetching required libraries"
+
+python -m pip install --update pip
+pip install pymupdf
+
+echo "Getting split.py"
+
+wget -quiet -O split.py https://raw.githubusercontent.com/northy/pdf-to-markdown-workflow/master/split.py
+
+echo "Running split.py"
 
 python split.py ${INPUT_PDF_INPUT} ${INPUT_OUTPUT_FOLDER} ${INPUT_SOURCE_MD} ${INPUT_OUTPUT_MD}
 
 rm split.py
 
-git config --local user.email "action@github.com"
-git config --local user.name "GitHub Action"
-git add ${INPUT_OUTPUT_FOLDER} ${INPUT_OUTPUT_MD}
-git commit -m "Compile MD"
+echo "Commiting and pushing"
+
+git add ${INPUT_OUTPUT_FOLDER} ${INPUT_OUTPUT_MD} > /dev/null
+git commit -m "Compile MD" --quiet
 
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-git push "${remote_repo}";
+git push "${remote_repo}" --quiet
+
+echo "Done!"
