4e4d56f584749565a22a33f277e8cd5fdd84c562
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2020-09-09 18:06:09 -0300

    🔧 Attempt to fix root folder

--- split.py
+++ split.py
@@ -4,11 +4,12 @@ pdf_input = sys.argv[1]
 output_folder = sys.argv[2]
 source_md = sys.argv[3]
 output_md = sys.argv[4]
+output_folder_root=output_folder
 if len(sys.argv)>5 :
-    output_folder = sys.argv[5]+output_folder
+    output_folder_root = sys.argv[5]+output_folder_root
 
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+if not os.path.exists(output_folder_root):
+    os.makedirs(output_folder_root)
 
 doc = fitz.open(pdf_input)
 source = open(source_md,'r',encoding='utf-8')
@@ -31,8 +32,9 @@ while True :
         page = doc.loadPage(i-1)
         pix = page.getPixmap()
         output = f"{output_folder}/page{i}.png"
+        output_root = f"{output_folder_root}/page{i}.png"
         imgs.append(f'![Page {i}]({output} "Page {i}")\n---\n')
-        pix.writePNG(output)
+        pix.writePNG(output_root)
     except :
         break
     i+=1
